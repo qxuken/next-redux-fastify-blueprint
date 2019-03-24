9b416097bdf8bd9bd89e2d7dfa5f4de4eb047d8c
Nasonov V <qxuken@gmail.com>
2019-03-24 20:10:34 +0300

    add console greetings and refactor README

--- README.md
+++ README.md
@@ -2,10 +2,12 @@
 
 ## todo
 
-- write examples
-- router
-- look into testing
 - look into typechecking
-- add note about manifest in the static folder
-- redux selectors
+- look into testing
+- router
 - api presenters
+- redux selectors
+- internationalization
+- demo of all features
+- write README.md
+- add note about manifest in the static folder
--- src/services/Console.js
+++ src/services/Console.js
@@ -1,7 +1,12 @@
 /* eslint-disable no-console */
 class Console {
   static greeting() {
-    this.log('No regrets');
+    this.log(
+      '%c+',
+      `font-size: 1px; padding: 95px 232px; line-height: 65px; background: url(${
+        process.env.FRONT_HOST
+      }/static/images/no-regrets.gif); background-size: 464px 260px; color: transparent;`,
+    );
   }
 
   static log() {
